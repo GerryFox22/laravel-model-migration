b2d17b39840250e9be97b57f412c82b64c4fb99e
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-11-04 14:30:39 +0100

    Implementazione e formattazione home blade

---
+++ laravel-model-migration/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Home</title>
+</head>
+<body>
+
+    <div class="container">
+    <h1 class="text-center p-5">Viaggi di Natale</h1>
+        <div class="row">
+        
+                <div class="col-3 pb-5 text-center">
+                    <h3 class="pb-4">Titolo Viaggio</h3>
+                    <p><span class="text-danger">Prezzo :</span>  </p>
+                    <p><span class="text-danger">Durata: </span></p>
+                    <p><span class="text-danger">Partenze: </span></p>
+                    <p><span class="text-danger">Min Partecipanti: </span></p>
+                    <p><span class="text-danger">Descrizione: </span></p>
+                </div>
+            
+         
+        </div>
+    </div>
+        
+   
+</body>
+</html>
